709724412ffb9557c7362bc90a28ac0289d8e72c
Brian Yoder <brianyoder@gmail.com>
2023-02-24 11:58:06 -0800

    add Show orders controller

--- BakeryWholesale/Controllers/OrdersController.cs
+++ BakeryWholesale/Controllers/OrdersController.cs
@@ -16,5 +16,16 @@ namespace BakeryWholesale.Controllers
         return View(vendor);
     }
 
+    [HttpGet("vendors/{vendorId}/orders/{orderId}")]
+    public ActionResult Show(int vendorId, int orderId)
+    {
+      Order order = Order.Find(orderId);
+      Vendor vendor = Vendor.Find(vendorId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("order", order);
+      model.Add("vendor", vendor);
+      return View(model);
+    }
+
   }
 }
