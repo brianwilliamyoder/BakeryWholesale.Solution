a7306ad0aa0a520c818c91a2c001409b39da196d
Brian Yoder <brianyoder@gmail.com>
2023-03-04 17:43:49 -0800

    refactor order tests with [TestInitialize]

--- BakeryWholesale.Tests/ModelTests/OrderTests.cs
+++ BakeryWholesale.Tests/ModelTests/OrderTests.cs
@@ -6,64 +6,68 @@ using System.Collections.Generic;
 
 namespace BakeryWholesale.Tests
 {
+  
   [TestClass]
   public class OrderTests : IDisposable
   {
+
     public void Dispose()
+  {
+    Order.ClearAll();
+  }
+
+  private DateTime _firstTestDate;
+  private DateTime _secondTestDate;
+  private Order _firstOrder;
+  private Order _secondOrder;
+  
+  
+    [TestInitialize]
+    public void Initialize()
     {
-      Order.ClearAll();
+      _firstTestDate = new DateTime(2023, 4, 5);
+      _secondTestDate = new DateTime(2023, 5, 13);
+      _firstOrder = new Order ("May order", "5 loaves", 15, _firstTestDate);
+      _secondOrder = new Order("April Order", "Pastries", 300, _secondTestDate);
     }
+
 
     [TestMethod]
     public void OrderConstructor_CreateInstanceOfOrder_Order()
     {
-      Order newOrder = new Order("May order", "5 loaves", 15, "5/1/22");
-      Assert.AreEqual(typeof(Order), newOrder.GetType());
+      Assert.AreEqual(typeof(Order), _firstOrder.GetType());
     }
 
     [TestMethod]
     public void OrderConstructor_ReturnsTitle_String()
     {
-      string title = "May Order";
-      Order newOrder = new Order (title, "Bread, 5 loaves", 15, "5/1/22" );
-      Assert.AreEqual(title, newOrder.Title);
+      String title = new String("May order");
+      Assert.AreEqual(title, _firstOrder.Title);
       
     }
 
     [TestMethod]
-    public void GetAll_ReturnsEmptyList_OrderList()
+    public void GetAll_ReturnsList_OrderList()
     {
-      List<Order> newList = new List<Order> { };
+      List<Order> newList = new List<Order> {_firstOrder, _secondOrder};
       List<Order> result = Order.GetAll();
+      Console.WriteLine(newList);
       CollectionAssert.AreEqual(newList, result);
     }
-  
-    [TestMethod]
-    public void GetAll_ReturnsOrderList_OrderList()
-    {
-      Order newOrder1 = new Order("April Order", "Pastries", 300, "4/1/22");
-      Order newOrder2 = new Order("March Order", "Bread", 5, "3/1/22");
-      List<Order> newList = new List<Order> { newOrder1, newOrder2 }; 
-      List<Order> result = Order.GetAll();
-      CollectionAssert.AreEqual(result, newList);
-    }
+
 
     [TestMethod]
     public void GetId_ItemAssignedId_Int()
     {
-      Order newOrder1 = new Order("April Order", "Pastries", 300, "4/1/22");
-      int result = newOrder1.Id;
       int comparison = 1;
-      Assert.AreEqual(comparison, result);
+      Assert.AreEqual(comparison, _firstOrder.Id);
     }
 
     [TestMethod]
     public void Find_ReturnsOrder_Order()
     {
-      Order newOrder1 = new Order("April Order", "Pastries", 300, "4/1/22");
-      Order newOrder2 = new Order("March Order", "Bread", 5, "3/1/22");
       Order result = Order.Find(2);
-      Assert.AreEqual(result, newOrder2);
-    }
+      Assert.AreEqual(result, _secondOrder);
+    } 
   }
 }
